1fa94e28cfc448da00591b5d0f00882957434a37
arzynik <github@arzynik.com>
2012-12-21 17:40:52 -0800

    hella progress

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,6 @@
-var App = {}, Template = {};
+var App = {
+	loc: {}
+}, Template = {};
 
 if (document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1 ) {
 	// phonegap
@@ -39,7 +41,7 @@ var initMain = function() {
 	closeMask.addEventListener('touchmove', function(event) {
 		event.preventDefault();
 	});
-	
+
 	page.addEventListener('touchmove', function(event) {
 		console.log(event)
 	});
@@ -190,11 +192,10 @@ var initMain = function() {
 
 
 $(function() {
-	/*
-	$('.nav-news').live('click', function() {
-		App.router.navigate('/news', true);
+	App.db = window.openDatabase('crunchbutton', '1.0', 'Preferences', 1000000);
+	App.db.transaction(function(tx) {
+		tx.executeSql('CREATE TABLE IF NOT EXISTS settings (id unique, data)');
 	});
-	*/
 
 	var User = Backbone.Model.extend({
 		idAttribute: 'id_user',
@@ -208,7 +209,7 @@ $(function() {
 	});
 
 	var Restaurant = Backbone.Model.extend({
-		idAttribute: 'id_restaurant',
+		idAttribute: 'permalink',
 		localStorage: new Backbone.LocalStorage('Restaurant')
 	});
 
@@ -227,7 +228,7 @@ $(function() {
 			initMain();
 
 			App.navigator = new BackStack.StackNavigator({
-				el: '#page'
+				el: '#body'
 			});
 			//App.navigator.defaultPushTransition = new BackStack.NoEffect();
 			Backbone.history.start({pushState: true});
@@ -240,33 +241,26 @@ $(function() {
 	
 	var HomeView = Backbone.View.extend({
 		render:function (er) {
+			window.scrollTo(0, 1);
+
 			this.$el.html(this.template());
 //			$('#body').html();
 			//$('#page header').hide();
 			//menuEnabled = false;
-
-			$('.eat').live('click',function() {
-				App.router.navigate('/restaurants', {trigger: true});
-			});
-
-			$('input').bind('blur', function(e) {
-				window.scrollTo(0, 1);
-			});
-
-			$(window).bind('resize', function(e) {
-				//window.scrollTo(0, 1);
-			});
-
-			$('#page').css('overflow','hidden');
 		},
 		events: {
+			'click .eat': 'eatClick',
 			'blur input': 'scrollTop'
 		},
 		scrollTop: function() {
 			window.scrollTo(0, 1);
 		},
 		eatClick: function(e) {
-			App.router.navigate('/restaurants', {trigger: true});
+			App.setLocation(function() {
+				App.getRestaurants(function() {
+					App.router.navigate('/restaurants', {trigger: true});
+				});
+			});
 		},
 		initialize: function (args) {
 			this.template = _.template(Template.home);
@@ -275,15 +269,58 @@ $(function() {
 
 	var RestaurantsView = Backbone.View.extend({
 		render : function() {
-			this.$el.html(this.template());
+			var self = this;
+			window.scrollTo(0, 1);
+			
+			this.$el.css('height', '');
+
+			this.$el[0].addEventListener('webkitTransitionEnd', function(e) {
+				$('#body').css({
+					height: self.$el[0].scrollHeight
+				});
+			}, false);
+
+			this.$el.html(this.template({
+				restaurants: App.restaurants
+			}));
 			//$('#page header').show();
 			//menuEnabled = true;
+		},
+		events: {
+			'click .restaurant': 'restaurantClick',
+		},
+		restaurantClick: function(e) {
+			e.stopPropagation();
+			e.preventDefault();
+			
+			var permalink = $(e.currentTarget).attr('data-permalink');
+			
+			App.router.navigate('/restaurant/' + permalink, {trigger: true});
+			return;
+
+
+			return false;
+			
 		},
 		initialize: function (args) {
 			this.template = _.template(Template.restaurants);
 		}
 	});
 	
+	var RestaurantView = Backbone.View.extend({
+		render : function() {
+			var self = this;
+			window.scrollTo(0, 1);
+
+			this.$el.html(this.template({
+				restaurant: App.restaurant
+			}));
+		},
+		initialize: function (args) {
+			this.template = _.template(Template.restaurant);
+		}
+	});
+
 	/*
 	var ErrorView = Backbone.View.extend({
 		el: $('body'),
@@ -329,17 +366,28 @@ $(function() {
 			'community/:id': 'getEvent',
 			'forceupdate': 'getUpdate',
 			'restaurants': 'getRestaurants',
+			'restaurant/:permalink': 'getRestaurant',
 			'*actions': 'defaultRoute',
 		}
 	});
-	
+
 	App.router = new AppRouter;
 		App.router.on('route:defaultRoute', function(actions) {
 		App.navigator.pushView(HomeView);
 	});
-	
+
 	App.router.on('route:getRestaurants', function(actions) {
 		App.navigator.pushView(RestaurantsView);
+	});
+	
+	App.router.on('route:getRestaurant', function(actions) {
+		var rest = new Restaurant({permalink: arguments[0]});
+		rest.fetch({
+			success: function (rest) {
+				App.restaurant = rest;
+				App.navigator.pushView(RestaurantView);
+			}
+		});
 	});
 
 	var AppInit = function(res) {
@@ -362,17 +410,15 @@ $(function() {
 		var tpl = ['root','home','restaurants'];
 		var requests = [];
 
-		$.when($.get('view/root.html'), $.get('view/home.html'), $.get('view/restaurants.html')).done(function() {
+		$.when($.get('view/root.html'), $.get('view/home.html'), $.get('view/restaurants.html'), $.get('view/restaurant.html')).done(function() {
 			Template.root = arguments[0][0];
 			Template.home = arguments[1][0];
 			Template.restaurants = arguments[2][0];
+			Template.restaurant = arguments[3][0];
 
 			var main = new MainView;
 			main.render();
 		});
-		
-
-
 	};
 
 	var AppUpdate = function(res) {
@@ -413,8 +459,74 @@ $(function() {
 	}).error(function() {
 		var main = new ErrorView;
 		main.render('The server is currently down. Sorry!');
-	})
+	});
+	
+	App.getRestaurants = function() {
+	
+		if (arguments.length == 2) {
+			var filters = arguments[0];
+			var complete = arguments[1];
+		} else {
+			var complete = arguments[0];	
+		}
+	
+		$.getJSON(server + 'restaurants', {lat: App.loc.lat, lon: App.loc.lon}, function(res, status) {
+			for (var x in res.restaurants) {
+				var rest = new Restaurant(res.restaurants[x]);
+				rest.save();
+			}
+			App.restaurants = res.restaurants;
+			complete();
+	
+		}).error(function(e) {
+			alert('Sorry! We could not retrieve restaurant list. Are you connected to the internet?')
+		})
+	};
+
 });
 
+App.setLocation = function(complete) {
+	var geocoder = new google.maps.Geocoder();
+	var forceLoc = null;
 
-// http://i.crunchr.co/53x53/wings-over-prov.jpg?crop=1
+	switch ($('input[name="input-where"]').val().toLowerCase()) {
+		case 'yale':
+		case 'new haven':
+			forceLoc = '41.310598, -72.927933';
+			break;
+		case 'harvard':
+		case 'cambridge':
+		case 'the game':
+		case 'hahvahd':
+		case 'boston':
+		case 'somerville':
+			forceLoc = '42.376173, -71.115757';
+			break;
+		case 'dc':
+		case 'gwu':
+		case 'gw':
+		case 'george washington':
+		case 'george washington university':
+		case 'the district':
+		case 'district of columbia':
+		case 'foggy bottom':
+		case 'georgetown':
+		case 'gu':
+		case 'georgetown university':
+			forceLoc = '38.8900, -77.0300';
+			break;
+	}
+	
+	geocoder.geocode({'address': forceLoc || $('input[name="input-where"]').val()}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			App.loc.lat = results[0].geometry.location.lat();
+			App.loc.lon = results[0].geometry.location.lng();
+			App.loc.address = results[0].formatted_address;
+			App.loc.entered = $('input[name="input-where"]').val();
+
+			complete();
+		} else {
+			$('input[name="input-where"]').val('').attr('placeholder','Oops! We couldn\'t find that address!');
+		}
+	});
+};
