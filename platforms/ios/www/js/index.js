6269654615e59bd884e49ea21150bca2ca0b2c78
arzynik <github@arzynik.com>
2013-08-03 11:53:38 -0700

    cleanup and added bundle builder

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,49 +1,132 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+var CB = {
+	server: 'http://beta.crunchr.co/',
+	init: function() {
+	
+	LazyCache.init({
+		debug: false,
+		storeComplete: null
+	});
+	
+$element.LazyCache({
+						attr: 'data-image',
+						lazy: true,
+						css: {
+							'background-position': 'center',
+							'background-repeat': 'no-repeat',
+							'background-size': 'cover'
+						}
+					});
+	
+	
+		CB.version = localStorage.version;
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+		$.getJSON(CB.service + 'api/config', function(r) {
+			CB.config = r;
+			if (CB.version != CB.config.version) {
+				// update all the assets
+				getAssets(CB.buildApp);
+			} else {
+				CB.buildApp();
+			}
+		});
+		
+	},
+	getAssets: function(cb) {
+		$.when($.ajax(CB.server + 'assets/bundle.js?v=' + CB.config.version), $.ajax(CB.server + 'assets/bundle.css?v=' + CB.config.version)).then(function(){
+			console.log(arguments);
+			if (typeof cb === 'function') {
+				cb();
+			}
+		});
 
-        console.log('Received Event: ' + id);
-    }
+	},
+	buildApp: function() {
+		alert('built');
+	}
 };
+
+var TapToScroll = function() {
+  
+}
+
+TapToScroll.prototype.initListener = function() {
+	cordova.exec(null, null, 'TapToScroll', 'initListener',[]);
+};
+
+if (!window.plugins) {
+	window.plugins = {};
+}
+if (!window.plugins.tapToScroll) {
+	window.plugins.tapToScroll = new TapToScroll();
+}
+
+var login = function() {
+	FB.login(status, {scope: 'email'});
+};
+
+document.addEventListener('deviceready', function() {
+
+	CB.init();
+
+	window.plugins.tapToScroll.initListener();
+	window.addEventListener('statusTap', function() {
+		$('html, body').animate({scrollTop: 0}, 200);
+		//smoothTop();
+	});
+
+	var status = function(res) {
+		if (res.status === 'connected' && res.authResponse && res.authResponse.accessToken) {
+			FB.api('/me', {fields: 'name' }, function(me) {
+				// logged in
+				console.log('ME',me);
+			});
+		}
+	}
+	
+	FB.Event.subscribe('auth.statusChange', status);
+
+	//FB.getLoginStatus(status);
+	
+
+	FB.init({
+		appId: '411729638889643',
+		//cookie: true,
+		//xfbml: true,
+		//oauth: true,
+		nativeInterface: CDV.FB,
+		useCachedDialogs: false
+	});
+
+	
+}, false);
+//location.href="http://beta.crunchr.co/";
+
+
+
+
+
+
+
+
+var smoothTop = function() {
+	var pos = $(window).scrollTop();
+
+	$('body').css({
+		'margin-top': -pos + 'px',
+		'overflow-y': 'scroll',
+	});
+
+	$(window).scrollTop(0);
+
+	$('body').css({
+		'transition': 'margin-top .2s cubic-bezier(0.35, 0.71, 0.59, 0.88)',
+		'margin-top': '0'    	
+	});
+
+	$('body').on('webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd', function() {
+		$('body').css('transition', 'none');
+	});
+}
+
+
+
