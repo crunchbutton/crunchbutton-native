792cf07644cadf191a32372d609034054fe54f8d
arzynik <github@arzynik.com>
2013-08-03 11:53:38 -0700

    cleanup and added bundle builder

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-SRC_DIR="/home/"
-DST_DIR="./"
+DST_DIR="./platforms/ios/www/"
+SERVER="http://beta.crunchr.co/"
 
-cp -a "$SRC_DIR$subpath" "$DST_DIR$subpath"
+wget -O - --header="Accept-Encoding: gzip" "${SERVER}assets/js/bundle.js" | gunzip > "${DST_DIR}js/bundle.js"
+wget -O - --header="Accept-Encoding: gzip" "${SERVER}assets/css/bundle.css" | gunzip > "${DST_DIR}css/bundle.css"
