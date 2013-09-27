135dc153106823a7a52b5483f51b7daa63712f44
arzynik <github@arzynik.com>
2013-09-26 19:36:38 -0700

    added balanced lib

---
+++ platforms/ios/Crunchbutton/Plugins/BalancedPlugin.m
@@ -0,0 +1,32 @@
+
+
+#import "BalancedPlugin.h"
+#import <Cordova/CDV.h>
+
+
+@implementation BalancedPlugin
+
+- (void) tokenizeCard:(CDVInvokedUrlCommand*)command
+{
+    NSString* num = [command argumentAtIndex:0];
+    NSString* expmonth = [command argumentAtIndex:1];
+    NSString* expyear = [command argumentAtIndex:2];
+    NSString* cvv = @"";
+
+	BPCard *card = [[BPCard alloc] initWithNumber:num expirationMonth:expmonth.intValue expirationYear:expyear.intValue securityCode:cvv];
+	Balanced *balanced = [[Balanced alloc] initWithMarketplaceURI:@"/v1/marketplaces/TEST-MP87deQgmIr46omxRRF4KGO"];
+	
+	[balanced tokenizeCard:card onSuccess:^(NSDictionary *responseParams) {
+		// success
+		CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:responseParams];
+		[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+	} onError:^(NSError *error) {
+		// failure
+		CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+		[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+	}];
+}
+
+@end
+
+
---
+++ platforms/ios/Crunchbutton/Plugins/balanced/Balanced.h
@@ -0,0 +1,26 @@
+//
+//  Balanced.h
+//  Balanced iOS Example
+//
+//  Created by Ben Mills on 3/9/13.
+//
+
+#import <Foundation/Foundation.h>
+#import "BPCard.h"
+#import "BPBankAccount.h"
+
+__unused static NSString *BalancedResponseBrandKey = @"brand";
+__unused static NSString *BalancedResponseCardTypeKey = @"card_type";
+__unused static NSString *BalancedResponseHashKey = @"hash";
+__unused static NSString *BalancedResponseIdKey = @"id";
+__unused static NSString *BalancedResponseIsValidKey = @"is_valid";
+__unused static NSString *BalancedResponseUriKey = @"uri";
+
+typedef void (^BalancedTokenizeResponseBlock)(NSDictionary *responseParams);
+typedef void (^BalancedErrorBlock)(NSError *error);
+
+@interface Balanced : NSObject <NSURLConnectionDelegate>
+- (id)initWithMarketplaceURI:(NSString *)uri;
+- (void)tokenizeCard:(BPCard *)card onSuccess:(BalancedTokenizeResponseBlock)successBlock onError:(BalancedErrorBlock)errorBlock;
+- (void)tokenizeBankAccount:(BPBankAccount *)bankAccount onSuccess:(BalancedTokenizeResponseBlock)successBlock onError:(BalancedErrorBlock)errorBlock;
+@end
