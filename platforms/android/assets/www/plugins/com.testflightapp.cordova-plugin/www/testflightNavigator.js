c4a5eb56aa7db04a9b5ed4ee0589903f5422da94
Daniel Camargo <pererinha@gmail.com>
2014-04-28 10:02:48 -0300

    partial #2958 - removed testflight

--- platforms/android/assets/www/plugins/com.testflightapp.cordova-plugin/www/testflightNavigator.js
+++
@@ -1,9 +0,0 @@
-cordova.define("com.testflightapp.cordova-plugin.TestFlight", 
-	function(require, exports, module) {var exec = require('cordova/exec');
-
-module.exports = {
-    takeOff: function( success, error, appToken ) {
-        exec(success, error, 'TestFlightSDK', "takeOff", [ appToken ]);
-    },
-	};
-});
