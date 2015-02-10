26552a69e603f50b5530c7aaa8e17b44086a589e
Pererinha <pererinha@gmail.com>
2015-02-10 17:00:24 -0200

    android build working

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/pererinha/Dev/crunchbutton/phonegap/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/pererinha/Dev/crunchbutton/phonegap/platforms/android/CordovaLib/AndroidManifest.xml \
