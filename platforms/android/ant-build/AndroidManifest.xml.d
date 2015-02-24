1698754e500d9fe75eab892ddf92d71ef18f7410
Devin Smith <github@arzynik.com>
2015-02-24 09:29:02 -0800

    another full rebuild of android app which seems to work

--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/arzynik/Development/ios/cruchbutton-repo1/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/arzynik/Development/ios/cruchbutton-repo1/platforms/android/AndroidManifest.xml \
+/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/AndroidManifest.xml \
--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -1,6 +1,8 @@
-/Users/pererinha/Dev/crunchbutton/phonegap/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/Users/pererinha/Dev/crunchbutton/phonegap/platforms/android/ant-build/CordovaApp.ap_ \
-/Users/pererinha/Dev/crunchbutton/phonegap/platforms/android/ant-build/classes.dex \
-/Users/pererinha/Dev/crunchbutton/phonegap/platforms/android/CordovaLib/ant-build/classes.jar \
-/Users/pererinha/Dev/crunchbutton/phonegap/platforms/android/libs/gcm.jar \
-/Users/pererinha/Dev/crunchbutton/phonegap/platforms/android/libs/android-support-v13.jar \
+/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
+/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/ant-build/CordovaApp.ap_ \
+/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/ant-build/classes.dex \
+/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/CordovaLib/ant-build/classes.jar \
+/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/ant-build/classes.jar \
+/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/libs/bolts-android-1.1.2.jar \
+/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/libs/gcm.jar \
+/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/libs/android-support-v13.jar \
