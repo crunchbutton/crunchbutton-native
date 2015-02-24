16ab2b5c95de53d92c6a672809fee042be262653
Devin Smith <github@arzynik.com>
2015-02-24 09:29:02 -0800

    another full rebuild of android app which seems to work

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/arzynik/Development/ios/cruchbutton-repo1/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/arzynik/Development/ios/cruchbutton-repo1/platforms/android/CordovaLib/AndroidManifest.xml \
+/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/CordovaLib/AndroidManifest.xml \
---
+++ platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/ant-build/AndroidManifest.xml : \
+/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/AndroidManifest.xml \
