96df743a1f72a0387112c6e655ef33e947b3e5e0
Devin Smith <github@arzynik.com>
2015-02-25 12:37:48 -0800

    android updates

--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/AndroidManifest.xml \
+/Users/arzynik/Development/ios/cruchbutton-repo3/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/arzynik/Development/ios/cruchbutton-repo3/platforms/android/AndroidManifest.xml \
