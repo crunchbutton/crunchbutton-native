51d000ba7ca4ccca4ed0f25b6a1f91f4d99a46eb
Devin Smith <github@arzynik.com>
2015-02-25 15:19:42 -0800

    android loves to rebuild everything all the time

--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/arzynik/Development/ios/cruchbutton-repo3/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/arzynik/Development/ios/cruchbutton-repo3/platforms/android/AndroidManifest.xml \
+/Users/arzynik/Development/ios/cruchbutton-repo1/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/arzynik/Development/ios/cruchbutton-repo1/platforms/android/AndroidManifest.xml \
--- platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/bin/AndroidManifest.xml.d
+++ platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/bin/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/arzynik/Development/ios/cruchbutton-repo3/platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/ant-build/AndroidManifest.xml : \
-/Users/arzynik/Development/ios/cruchbutton-repo3/platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/AndroidManifest.xml \
+/Users/arzynik/Development/ios/cruchbutton-repo1/platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/ant-build/AndroidManifest.xml : \
+/Users/arzynik/Development/ios/cruchbutton-repo1/platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/AndroidManifest.xml \
