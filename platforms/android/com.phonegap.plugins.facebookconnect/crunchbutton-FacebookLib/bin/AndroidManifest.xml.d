f0545c7810926f64d463ffb3f8b6774280c54ae1
Devin Smith <github@arzynik.com>
2015-02-25 12:37:48 -0800

    android updates

--- platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/bin/AndroidManifest.xml.d
+++ platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/bin/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/ant-build/AndroidManifest.xml : \
-/Users/arzynik/Development/ios/cruchbutton-repo2/platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/AndroidManifest.xml \
+/Users/arzynik/Development/ios/cruchbutton-repo3/platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/ant-build/AndroidManifest.xml : \
+/Users/arzynik/Development/ios/cruchbutton-repo3/platforms/android/com.phonegap.plugins.facebookconnect/crunchbutton-FacebookLib/AndroidManifest.xml \
